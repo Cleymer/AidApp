3760ea557e2846fa08c4a56e38c869c9e408ef78
DMarcelo <david.saenz8818@est.uca.edu.ni>
2020-11-24 17:51:59 -0600

    MenuDrawer

--- app/src/main/java/com/teamponytta/aidapp/fragment/ValorarFragment.java
+++ app/src/main/java/com/teamponytta/aidapp/fragment/ValorarFragment.java
@@ -1,4 +1,4 @@
-package com.teamponytta.aidapp;
+package com.teamponytta.aidapp.fragment;
 
 import android.os.Bundle;
 
@@ -8,12 +8,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.teamponytta.aidapp.R;
+
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link nav_acercade#newInstance} factory method to
+ * Use the {@link ValorarFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class nav_acercade extends Fragment {
+public class ValorarFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +26,7 @@ public class nav_acercade extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public nav_acercade() {
+    public ValorarFragment() {
         // Required empty public constructor
     }
 
@@ -34,11 +36,11 @@ public class nav_acercade extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment nav_acercade.
+     * @return A new instance of fragment nav_valorar.
      */
     // TODO: Rename and change types and number of parameters
-    public static nav_acercade newInstance(String param1, String param2) {
-        nav_acercade fragment = new nav_acercade();
+    public static ValorarFragment newInstance(String param1, String param2) {
+        ValorarFragment fragment = new ValorarFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -59,6 +61,6 @@ public class nav_acercade extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_nav_acercade, container, false);
+        return inflater.inflate(R.layout.fragment_nav_valorar, container, false);
     }
 }
