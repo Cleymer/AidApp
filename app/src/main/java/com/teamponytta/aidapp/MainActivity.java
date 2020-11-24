03d7fab65a43b34a52d8514c128787ee70a4cb6a
DMarcelo <david.saenz8818@est.uca.edu.ni>
2020-11-24 17:45:53 -0600

    MenuDrawer

--- app/src/main/java/com/teamponytta/aidapp/MainActivity.java
+++ app/src/main/java/com/teamponytta/aidapp/MainActivity.java
@@ -32,7 +32,8 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_primerosAuxilios, R.id.nav_botiquin, R.id.nav_infoEmergencia)
+                R.id.nav_home, R.id.nav_primerosAuxilios, R.id.nav_botiquin,
+                R.id.nav_infoEmergencia, R.id.nav_compartir, R.id.nav_valorar, R.id.nav_acercade)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
