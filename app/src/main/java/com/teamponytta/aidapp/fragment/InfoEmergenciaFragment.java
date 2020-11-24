b893d5d6afa4f3c54299fab09baf20c44e5739b1
Cleymer <cleymer.zavala8807@est.uca.edu.ni>
2020-11-23 23:13:12 -0600

    Fragment de Botiquin e Informacion de Contacto

--- app/src/main/java/com/teamponytta/aidapp/fragment/InfoEmergenciaFragment.java
+++ app/src/main/java/com/teamponytta/aidapp/fragment/InfoEmergenciaFragment.java
@@ -1,4 +1,4 @@
-package com.teamponytta.aidapp;
+package com.teamponytta.aidapp.fragment;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.teamponytta.aidapp.R;
 
 /**
  * A simple {@link Fragment} subclass.
