9fd102cb7487c013e2d579b2b1ba4f5af00b532b
Cleymer <cleymer.zavala8807@est.uca.edu.ni>
2020-12-03 01:39:51 -0600

    Info Fragment

--- app/src/main/java/com/teamponytta/aidapp/fragment/InfoFragment.java
+++ app/src/main/java/com/teamponytta/aidapp/fragment/InfoFragment.java
@@ -1,5 +1,6 @@
 package com.teamponytta.aidapp.fragment;
 
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 
@@ -7,12 +8,29 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.VideoView;
 
 import com.teamponytta.aidapp.R;
+import com.teamponytta.aidapp.model.Pastilla;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -20,6 +38,11 @@ import com.teamponytta.aidapp.R;
  * create an instance of this fragment.
  */
 public class InfoFragment extends Fragment {
+
+    private static final String TAG = "InfoFragment";
+
+    TextView txtDesc, txtPaso, txtNombre;
+    ImageView imgPa;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -65,18 +88,136 @@ public class InfoFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_info, container, false);
+
+        View vista = inflater.inflate(R.layout.fragment_info, container, false);
+
+        String pa = "";
+
+        if (getArguments()!=null){
+            pa = getArguments().getString("name");
+        }
+
+        TextView tv = vista.findViewById(R.id.tv_name);
+        tv.setText(pa);
+
+        setContent(pa, vista);
+
+        return vista;
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        //TextView txt = view.findViewById(R.id.tv_ejemplo);
-
-        if (getArguments()!=null){
-            String nombre = getArguments().getString("name");
-            //txt.setText(nombre);
-        }
 
     }
+
+    private void setContent(String name_pa, View view)
+    {
+        try {
+            String jsonDataString = readJSONDataFromFile();
+            JSONArray jsonArray = new JSONArray(jsonDataString);
+
+            for (int i=0; i<=jsonArray.length(); i++){
+                JSONObject itemObject = jsonArray.getJSONObject(i);
+                String name = itemObject.getString("name");
+                String desc = itemObject.getString("description");
+                String paso = itemObject.getString("paso");
+
+                if (name.equals(name_pa)){
+                    txtDesc = view.findViewById(R.id.tv_desc);
+                    txtDesc.setText(desc);
+
+                    txtPaso = view.findViewById(R.id.pasos);
+                    txtPaso.setText(paso);
+
+                    imgPa = view.findViewById(R.id.img_pa);
+                    imgPa.setImageResource(searchPhoto(name_pa));
+
+                }
+            }
+
+        } catch (JSONException | IOException e) {
+            Log.d(TAG, "addItemsFromJSON: ", e);
+        }
+    }
+
+    private String readJSONDataFromFile() throws IOException{
+        InputStream inputStream = null;
+        StringBuilder builder = new StringBuilder();
+
+        try {
+            String jsonString = null;
+            inputStream = getResources().openRawResource(R.raw.pasopa);
+            BufferedReader bufferedReader = new BufferedReader(
+                    new InputStreamReader(inputStream, "UTF-8")
+            );
+
+            while ((jsonString = bufferedReader.readLine()) != null)
+            {
+                builder.append(jsonString);
+            }
+        }finally {
+            if (inputStream != null)
+            {
+                inputStream.close();
+            }
+        }
+
+        return new String(builder);
+    }
+
+
+    private int searchPhoto(String nombre){
+
+        int photo = R.drawable.acidez;
+
+        switch (nombre){
+            case "Hemorragias":
+                photo = R.drawable.hemorragias2;
+                return photo;
+
+            case "Asfixia":
+                photo = R.drawable.asfixia2;
+                return photo;
+
+            case "Shock":
+                photo = R.drawable.shock2;
+                return photo;
+
+            case "Contusiones":
+                photo = R.drawable.contusiones2;
+                return photo;
+
+            case "Heridas":
+                photo = R.drawable.heridas2;
+                return photo;
+
+            case "Esguince":
+                photo = R.drawable.esguince2;
+                return photo;
+
+            case "Fractura":
+                photo = R.drawable.fractura2;
+                return photo;
+
+            case "Quemaduras":
+                photo = R.drawable.quemaduras2;
+                return photo;
+
+            case "Parada Cardiorespiratoria":
+                photo = R.drawable.pcardiorespiratoria;
+                return photo;
+
+            case "Reanimacion Cardiopulmonar Básica":
+                photo = R.drawable.rcp2;
+                return photo;
+
+            default:
+                break;
+
+        }
+
+        return photo;
+    }
+
 }
