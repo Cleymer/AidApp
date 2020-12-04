7bbb38165431450f0d6cd8136687be7aac3f9ca5
DMarcelo <david.saenz8818@est.uca.edu.ni>
2020-12-04 04:14:20 -0600

    Test

--- app/src/main/java/com/teamponytta/aidapp/fragment/TestFragment.java
+++ app/src/main/java/com/teamponytta/aidapp/fragment/TestFragment.java
@@ -1,5 +1,7 @@
 package com.teamponytta.aidapp.fragment;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +9,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.teamponytta.aidapp.R;
 
@@ -15,7 +18,7 @@ import com.teamponytta.aidapp.R;
  * Use the {@link TestFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class TestFragment extends Fragment {
+public class TestFragment extends Fragment implements View.OnClickListener {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -25,6 +28,10 @@ public class TestFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    View vista;
+    Button nivel1, nivel2, nivel3;
+
 
     public TestFragment() {
         // Required empty public constructor
@@ -61,6 +68,37 @@ public class TestFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_test, container, false);
+
+        vista = inflater.inflate(R.layout.fragment_test, container, false);
+        nivel1 = vista.findViewById(R.id.nivel1);
+        nivel2 = vista.findViewById(R.id.nivel2);
+        nivel3 = vista.findViewById(R.id.nivel3);
+
+        nivel1.setOnClickListener(this);
+        nivel2.setOnClickListener(this);
+        nivel3.setOnClickListener(this);
+
+        return vista;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.nivel1:
+                gotoUrl("https://docs.google.com/forms/d/e/1FAIpQLSe37qyRCgkva5vQ4EUmvnyeahzS8wFVzPSRuGVRgey1SPm9-Q/viewform?usp=sf_link");
+                break;
+
+            case R.id.nivel2:
+                gotoUrl("https://forms.gle/eJxVGohDdPhfn7qj8");
+                break;
+
+            case R.id.nivel3:
+                gotoUrl("https://forms.gle/vvLLNnFUxcj1cxpR7");
+                break;
+        }
+    }
+    private void gotoUrl(String s){
+        Uri uri = Uri.parse(s);
+        startActivity(new Intent(Intent.ACTION_VIEW, uri));
     }
 }
